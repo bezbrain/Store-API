4b27bd2dafd1a1cb228945f264946c6bca9df4b9
bezbrain <belloquadriolawale@gmail.com>
2023-12-08 19:04:57 +0100

    Implement the search by featured, company and name

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,12 +1,26 @@
 const ProductsCollection = require("../models/Products");
 
 const getAllProducts = async (req, res) => {
-  const products = await ProductsCollection.find(req.query);
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+
+  // Use this condition to confirm that the key, 'feature' exist before sorting
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" }; // This object value is to make sure that whatever letters are typed, it displays product names with those letters where "i" means, case insensitivity
+  }
+  // console.log(queryObject);
+  const products = await ProductsCollection.find(queryObject);
   res.status(200).json({
     success: true,
     nbHits: products.length,
     products,
-    message: `Fetched all featured set to ${req.query.featured}`,
+    message: `Fetched data`,
   });
 };
 
